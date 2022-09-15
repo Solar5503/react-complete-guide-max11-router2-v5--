430da5e4462f9e17064e1417eca6373d5886242e
Solar5503 <spidervvv@mail.ru>
2022-09-15 20:58:29 +0300

    add 404 page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import QuoteList from '../components/quotes/QuoteList';
+
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
   { id: 'q2', author: 'Vadim', text: 'Learning English is good!' },
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Route, useParams } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Vadim', text: 'Learning English is good!' },
+];
 
 const QuoteDetail = () => {
   const { quoteId } = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+  if (!quote) return <p>No quote found!</p>;
+
   return (
     <>
-      <h1>Quote Detail Page {quoteId}</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
