80da7004ac2f6db440280898d2af39e6076f1493
Solar5503 <spidervvv@mail.ru>
2022-09-22 17:39:25 +0300

    add sending and getting quote data via http

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
   const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  useEffect(() => {
+    if (status === 'completed') history.push('/quotes');
+  }, [status, history]);
 
-    history.push('/quotes');
-  };
+  const addQuoteHandler = (quoteData) => sendRequest(quoteData);
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
